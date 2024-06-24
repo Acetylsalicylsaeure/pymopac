6109473d28aac38b7680c3d92f46a769350c256a
Acetylsalicylsaeure <110168604+Acetylsalicylsaeure@users.noreply.github.com>
2024-06-24 20:46:40 +0200

    added docs and verbose

--- pymopac/classes.py
+++ pymopac/classes.py
@@ -9,6 +9,21 @@ except:
     MOPAC_PATH = "mopac"
 
 
+def GeometryToMol(geometry):
+    """
+    Helper function that tries to infer a geometry from any input and returns a
+    rdkti Mol
+    """
+    if isinstance(geometry, str):
+        try:
+            mol = Chem.MolFromSmiles(geometry)
+        except Exception as e:
+            raise RuntimeError("Failed to parse input as SMILES", e)
+    elif isinstance(geometry, Chem.rdchem.Mol):
+        mol = geometry
+    return mol
+
+
 class MopacInput():
     """
     Class that sets up the input file for a MOPAC calculation.
@@ -22,31 +37,39 @@ class MopacInput():
         + TODO more
     AddHs: bool
         calls AddHs on the mol object
+
     preopt: bool
         uses MMFF to optimize the mol structure
+
     model: str
         all supported MOPAC keywords, e.g. AM1, PM6, ...
+
     comment: str
         second line in the input file
+
     run: bool
         directly runs the MOPAC calculation with init
+
     path:
         if False, a dir is created under /tmp/, else under the given str
+
+    verbose: bool
+        if True, prints additional class internal statements
+
+    stream: bool
+        if True, streams the outfile to stdout
 
     """
 
     def __init__(self, geometry, AddHs: bool = False, preopt: bool = False,
                  model: str = "PM7", comment: str = "#", run: bool = False,
-                 path=False):
+                 path=False, verbose: bool = False, stream: bool = False):
         self.model = model
         self.comment = comment
-        if isinstance(geometry, str):
-            try:
-                self.mol = Chem.MolFromSmiles(geometry)
-            except Exception as e:
-                raise RuntimeError("Failed to parse input as SMILES", e)
-        if isinstance(geometry, Chem.rdchem.Mol):
-            self.mol = geometry
+        self.verbose = verbose
+        self.stream = stream
+
+        self.mol = GeometryToMol(geometry)
 
         if AddHs:
             self.mol = AllChem.AddHs(self.mol)
@@ -59,6 +82,8 @@ class MopacInput():
             self.tmp_dir = f"/tmp/pymopac_{ts}"
             if not os.path.isdir(self.tmp_dir):
                 os.mkdir(self.tmp_dir)
+                if self.verbose:
+                    print(f"created path at {self.tmp_dir}")
         else:
             self.tmp_dir = path
             if not os.path.isdir(self.tmp_dir):
@@ -78,13 +103,27 @@ class MopacInput():
         inp = "\n".join([header, self.comment, xyz])
         return inp
 
-    def run(self):
+    def run(self, verbose: bool = None, stream: bool = None):
         """
         runs MOPAC as a subprocess
         returns MopacOutput class
+
+        verbose: bool
+            if True, prints additional class internal statements
+
+        stream: bool
+            if True, streams the outfile to stdout
         """
+        if verbose is None:
+            verbose = self.verbose
+        if stream is None:
+            stream = self.stream
+
         with open(f"{self.tmp_dir}/pymopac.mop", "w") as file:
             file.write(self.inpfile())
+        if verbose:
+            print(f"input file written to {self.tmp_dir}/pymopac.mop")
+
         os.chdir(self.tmp_dir)
         process = subprocess.run(
             [MOPAC_PATH, "pymopac.mop"],
@@ -115,7 +154,6 @@ class MopacOutput():
 
 
 if __name__ == "__main__":
-    inp = MopacInput("CCC", AddHs=True, preopt=True)
+    inp = MopacInput("CCC", AddHs=True, preopt=True, verbose=True)
     print(inp)
     out = inp.run()
-    print(out.outfile)
