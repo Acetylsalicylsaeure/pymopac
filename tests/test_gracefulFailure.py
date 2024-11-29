f4f4617a18503158868bbe8263eeeebf80db6374
Acetylsalicylsaeure <110168604+Acetylsalicylsaeure@users.noreply.github.com>
2024-11-29 15:32:31 +0200

    overlap test between API and classic mopac

--- tests/test_gracefulFailure.py
+++ tests/test_gracefulFailure.py
@@ -1,6 +1,9 @@
 import pymopac
 from rdkit import Chem
 from rdkit.Chem import AllChem
+import pytest
+import warnings
+
 
 mol = Chem.MolFromSmiles("[K][K]")
 AllChem.EmbedMolecule(mol)
@@ -9,7 +12,14 @@ for i in [0, 1]:
     conf.SetAtomPosition(i, [0, 0, 0])
 
 
-def test_overlap():
+@pytest.fixture
+def ignore_warnings():
+    with warnings.catch_warnings():
+        warnings.simplefilter("ignore")
+        yield
+
+
+def test_overlap_with_fixture(ignore_warnings):
     infile = pymopac.MopacInput(mol, preopt=False, aux=False)
     out = infile.run()
     assert isinstance(out, pymopac.MopacOutput)
